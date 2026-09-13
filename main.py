58f0e33deba8af5d8a9b8b37c4094278308adbbf
Ahmad nurrofik <ahmadnurrofikhrp17@gmail.com>
2026-09-13 12:18:47 +0700

    Perbaikan Procfile dan Port untuk Render

--- main.py
+++ main.py
@@ -74,5 +74,6 @@ def tambah_tugas():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    # Jalankan server lokal Autron
-    app.run(debug=True)
+    # Membaca PORT otomatis dari server Render, jika tidak ada pakai port 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
